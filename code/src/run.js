9aad5a390376e300648f62e6876cc9dfada26eb5
v-dev-cl <victor.amp.fern@gmail.com>
2022-02-22 11:17:17 -0300

    Update run.js

--- code/src/run.js
+++ code/src/run.js
@@ -5,7 +5,7 @@
   {
     // Constantes
     const VERSION = 4;
-    const EVENTO = 'biz175';
+    const EVENTO = 'biz175b';
     const NUM_FECHA = 2;
     const TIPO_TICKET_ID_MIN = 10;
     const TIPO_TICKET_ID_MAX = 12;
